1d62de969f1dc2f36f49b6f71447fe454287261f
UncleanlyClericr <junyatwin@gmail.com>
2019-07-23 11:23:46 -0700

    Set up the template

---
+++ foas_404/foas_404.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+This will pull a random "Fuck off as a service" request from the
+https://foaas.com/ website API, and insert variables as needed for the
+API request to be returned as a custom 404 page.
+
+:arguments:
+company
+name
+from
+reference
+
+:return: foas_404 and page template
+
+:requirements: pip install foaas; wsgi
+'''
