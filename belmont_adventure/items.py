eeadac07e7b9ea32887eaef0a228d3bf766c6274
UncleanlyClericr <junyatwin@gmail.com>
2019-07-31 13:05:47 -0700

    Okay, the beginnings of rooms

--- belmont_adventure/items.py
+++ belmont_adventure/items.py
@@ -24,104 +24,104 @@ class Item():
                                                    self.value)
 
 
-class Goldendickbutt(Item):
-    '''
-    Creating the Golden Dickbutt, combine the Golden Dickbutt with the skeletonkey
-    to exit the dungeon
-    '''
-    def __init__(self, amt):
-        self.amt = amt
-        super().__init__(name='Golden Dickbutt',
-                         description='A small statue of a penis, that has a \
+    class Goldendickbutt(Item):
+        '''
+        Creating the Golden Dickbutt, combine the Golden Dickbutt with the skeletonkey
+        to exit the dungeon
+        '''
+        def __init__(self, amt):
+            self.amt = amt
+            super().__init__(name='Golden Dickbutt',
+                             description='A small statue of a penis, that has a \
 penis on it\'s butt, stamped with {}. There is an opening under the second \
 penis'.format(str(self.amt)),
-                         value=self.amt)
+                             value=self.amt)
 
 
-class Skeletonkey(Item):
-    '''
-    The key to exit
-    '''
-    def __init__(self, amt):
-        self.amt = amt
-        super().__init__(name='Skeleton Key',
-                         description='A Skeleton Key with what appears to be \
-a highly detailed sphincter under a set of testicles, stamped with a {}'.format\
-                         (str(self.amt)),
-                         value=self.amt)
+    class Skeletonkey(Item):
+        '''
+        The key to exit
+        '''
+        def __init__(self, amt):
+            self.amt = amt
+            super().__init__(name='Skeleton Key',
+                             description='A Skeleton Key with what appears to be \
+    a highly detailed sphincter under a set of testicles, stamped with a {}'.format\
+                             (str(self.amt)),
+                             value=self.amt)
 
 
-class Weapon(Item):
-    '''
-    Weapons to attack
-    '''
-    def __init__(self, name, description, value, damage):
-        self.damage = damage
-        super().__init__(name, description, value)
+    class Weapon(Item):
+        '''
+        Weapons to attack
+        '''
+        def __init__(self, name, description, value, damage):
+            self.damage = damage
+            super().__init__(name, description, value)
 
-    def __str__(self):
-        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name,
-                                                             self.description,
-                                                             self.value,
-                                                             self.damage)
-
-
-class Dagger(Weapon):
-    '''
-    Starting weapon, available in room one
-    '''
-    def __init__(self):
-        super().__init__(name='Dagger',
-                         description='A small dagger, suitable for stabbing.',
-                         value=5,
-                         damage=5)
+        def __str__(self):
+            return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name,
+                                                                 self.description,
+                                                                 self.value,
+                                                                 self.damage)
 
 
-class Sword(Weapon):
-    '''
-    A stronger weapon to defeat the final boss
-    '''
-    def __init__(self):
-        super().__init__(name='Sword',
-                         description='A broad sword inscribed with the latin for\
-"Put this in your ass".',
-                         value=20,
-                         damage=20)
+    class Dagger(Weapon):
+        '''
+        Starting weapon, available in room one
+        '''
+        def __init__(self):
+            super().__init__(name='Dagger',
+                             description='A small dagger, suitable for stabbing.',
+                             value=5,
+                             damage=5)
 
 
-class Sax(Weapon):
-    '''
-    A stun weapon, used to stun the final boss
-    '''
-    def __init__(self):
-        super().__init__(name='Sax',
-                         description='A saxophone stamped with "Toot toot time to die".',
-                         value=10,
-                         damage=10)
+    class Sword(Weapon):
+        '''
+        A stronger weapon to defeat the final boss
+        '''
+        def __init__(self):
+            super().__init__(name='Sword',
+                             description='A broad sword inscribed with the latin for\
+    "Put this in your ass".',
+                             value=20,
+                             damage=20)
 
 
-class Dementorwasp(Weapon):
-    '''
-    The final weapon used to defeat the boss
-    '''
-    def __init__(self):
-        super().__init__(name='Demntor Wasp',
-                         description='A very angry wasp, in a jar.  It constantly\
- tries to coerce you into doing cocaine and murder in between singing gregorian \
- chants backwards.',
-                         value=666,
-                         damage=15)
+    class Sax(Weapon):
+        '''
+        A stun weapon, used to stun the final boss
+        '''
+        def __init__(self):
+            super().__init__(name='Sax',
+                             description='A saxophone stamped with "Toot toot time to die".',
+                             value=10,
+                             damage=10)
 
 
-class Whiskey(Weapon):
-    '''
-    The weapon of Jesse
-    '''
-    def __init__(self):
-        super().__init__(name='Sax',
-                         description='A half empty whiskey bottle labelled "Confidence"',
-                         value=10,
-                         damage=50)
+    class Dementorwasp(Weapon):
+        '''
+        The final weapon used to defeat the boss
+        '''
+        def __init__(self):
+            super().__init__(name='Demntor Wasp',
+                             description='A very angry wasp, in a jar.  It constantly\
+     tries to coerce you into doing cocaine and murder in between singing gregorian \
+     chants backwards.',
+                             value=666,
+                             damage=15)
+
+
+    class Whiskey(Weapon):
+        '''
+        The weapon of Jesse
+        '''
+        def __init__(self):
+            super().__init__(name='Sax',
+                             description='A half empty whiskey bottle labelled "Confidence"',
+                             value=10,
+                             damage=50)
 
 
 class Enemy:
@@ -140,49 +140,49 @@ class Enemy:
         return self.hp > 0
 
 
-class James(Enemy):
-    '''
-    Why
-    '''
-    def __init__(self):
-        super().__init__(name='James the Red', hp=30, damage=20)
+    class James(Enemy):
+        '''
+        Why
+        '''
+        def __init__(self):
+            super().__init__(name='James the Red', hp=30, damage=20)
 
 
-class Paul(Enemy):
-    '''
-    am
-    '''
-    def __init__(self):
-        super().__init__(name='Paul the Uncanny', hp=30, damage=15)
+    class Paul(Enemy):
+        '''
+        am
+        '''
+        def __init__(self):
+            super().__init__(name='Paul the Uncanny', hp=30, damage=15)
 
 
-class Aidan(Enemy):
-    '''
-    I
-    '''
-    def __init__(self):
-        super().__init__(name='Aidan the British', hp=30, damage=10)
+    class Aidan(Enemy):
+        '''
+        I
+        '''
+        def __init__(self):
+            super().__init__(name='Aidan the British', hp=30, damage=10)
 
 
-class Gwen(Enemy):
-    '''
-    docstringing
-    '''
-    def __init__(self):
-        super().__init__(name='Gwenny the Technicolor Shark', hp=30, damage=10)
+    class Gwen(Enemy):
+        '''
+        docstringing
+        '''
+        def __init__(self):
+            super().__init__(name='Gwenny the Technicolor Shark', hp=30, damage=10)
 
 
-class Talbot(Enemy):
-    '''
-    these?
-    '''
-    def __init__(self):
-        super().__init__(name='Talbot, the Talbort', hp=30, damage=15)
+    class Talbot(Enemy):
+        '''
+        these?
+        '''
+        def __init__(self):
+            super().__init__(name='Talbot, the Talbort', hp=30, damage=15)
 
 
-class Jesse(Enemy):
-    '''
-    Faaarrrrrrrrrrrrrrrrrrrrrrrrrrrrt
-    '''
-    def __init__(self):
-        super().__init__(name='Jesse the Far Too Sure of Himself', hp=60, damage=10)
+    class Jesse(Enemy):
+        '''
+        Faaarrrrrrrrrrrrrrrrrrrrrrrrrrrrt
+        '''
+        def __init__(self):
+            super().__init__(name='Jesse the Far Too Sure of Himself', hp=60, damage=10)
---
+++ belmont_adventure/tiles.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+'''
+Creating the tiles for the coordinate plane.
+'''
+#pylint: disable = C0103, R0903, R0201, W0107
+from belmont_adventure.items import Item
+from belmont_adventure.items import Enemy
+
+
+class MapTile:
+    '''
+    Defining tileset
+    '''
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class StartingRoom(MapTile):
+    '''
+    Defining the very start of the game.
+    '''
+    def intro_text(self):
+        '''
+        Here there be intro text
+        '''
+        return
+        '''
+        You find yourself if a cave with a flickering torch on the wall.
+        You can make out four paths, each equally as dark and foreboding.
+        '''
+
+
+    def modify_player(self, player):
+        '''
+        Nothing to be done.
+        '''
+        #Room has no action on player
+        pass
+
+
+class LootRoom(MapTile):
+    '''
+    This defines a room where an item can be picked up
+    '''
+    def __init__(self, x, y, item):
+        self.item = item
+        super().__init__(x, y)
+
+
+    def add_loot(self, player):
+        '''
+        Get rock.
+        '''
+        player.inventory.append(self.item)
+
+
+    def modify_player(self, player):
+        '''
+        Show inv, see gotten rock.  Yay...
+        '''
+        self.add_loot(player)
+
+
+class EnemyRoom(MapTile):
+    '''
+    This defines a room with an enemy.  Once the enemy is defeated it will
+    become a loot room.
+    '''
+    def __init__(self, x, y, enemy):
+        self.enemy = enemy
+        super().__init__(x, y)
+
+    def modify_player(self, the_player):
+        '''
+        TAKE DAMAGE!!!!
+        '''
+        if self.enemy.is_alive():
+            the_player.hp = the_player.hp - self.enemy.damage
+            print('Enemy does {} damage. You have {} HP remaining.'.format\
+            (self.enemy.damage, the_player.hp))
+
+
+class EmptyCavePath(MapTile):
+    '''
+    Gasp!  An empty room!
+    '''
+    def intro_text(self):
+        '''
+        Self explanatory
+        '''
+        return
+        '''
+        Another unremarkable part of the cave. You must forge onwards.
+        '''
+
+    def modify_player(self, player):
+        '''
+        Nothing to be done.
+        '''
+        #Room has no action on player
+        pass
+
+
+class Aidanroom(EnemyRoom):
+    '''
+    A room with a Brit!
+    '''
+    def __init__(self, x, y):
+        super().__init__(x, y, Enemy.Aidan())
+
+    def intro_text(self):
+        '''
+        Self explanatory
+        '''
+        if self.enemy.is_alive():
+            return
+            '''
+            Aiden britishly leaps at you from his liquor bottle filled corner.
+            In his hand flashes a golden statue of a penis.
+            '''
+
+        return
+        '''
+        The corpse of a dead englishman rots on the ground.
+        '''
+
+
+class FindDaggerRoom(LootRoom):
+    '''
+    A room with a dagger!
+    '''
+    def __init__(self, x, y):
+        super().__init__(x, y, Item.Dagger())
+
+    def intro_text(self):
+        '''
+        Self explanatory
+        '''
+        return
+        '''
+        Your notice something shiny in the corner.
+        It's a dagger! You pick it up.
+        '''
