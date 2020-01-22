fbf786bc531889339ac89c466629b731149baf30
UncleanlyClericr <junyatwin@gmail.com>
2020-01-22 13:34:48 -0800

    Hokay, so the math seems to check out.  I'll see in practice once everything else is done.

--- travel.py
+++ travel.py
@@ -1,4 +1,5 @@
  #!/usr/bin/env python3
+ # pylint: disable = C0103
 '''
 This is a first pass at writing a solar system transit time program.  The idea
 will be to compensate for orbits, varying speeds of constant thrust and time
@@ -22,7 +23,7 @@ days = traveltime / 86400
 tsc = travel time from the ship's point of reference
 acosh = inverse hyperbolic cosine
 
-tsc = 2 * (c / a) * math.acosh(d // 2 * k + 1) ** (-1)
+tsc = ((2 * c) / a ) * ( math.acosh * math.round( ( d / (2 * k ) ) + 1 ) )
 shipdays = tsc / 86400
 
 
@@ -34,3 +35,38 @@ problem has been solved by this point.  (Ex:  Epstien drive in The Expanse)
 
 '''
 import math
+import numpy as np
+
+
+def travel_time(a, d, k):
+    '''
+    Meat and potatoes of the back of the hand math to calculate travel time
+    '''
+    traveltime = 2 * math.sqrt(k / a * (d // 2 * k + 1) - 1)
+    return traveltime
+
+
+def ship_time(a, c, d, k):
+    '''
+    This is the same as above, only for ship time
+    '''
+    shiptime = np.arccosh(math.ceil((d / (2 * k))+ 1) * ((2 * c) / a))
+    return shiptime
+
+
+def output(a, d):
+    # a = 1
+    c = 2.99792458 * (10 ** 8)
+    # d = .5 * (1.495978707 * 10 ** 11)
+    k = c ** 2 / a
+
+    shiptime = ship_time(a, d, k)
+    traveltime = travel_time(a, c, d, k)
+    vmax = c / math.sqrt(1 + (k / a * ((traveltime / 2) ** 2)))
+
+    print('This trip will take {} days, {} days of ship time.  The maximum\
+          velocity reached will be {}.  Clocks will resync once docked').format\
+          (traveltime, shiptime, vmax)
+
+
+def menu():
