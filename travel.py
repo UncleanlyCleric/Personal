ca0c45ef16113e2ce64de01bc8cc99b8f285a615
UncleanlyClericr <junyatwin@gmail.com>
2020-01-23 13:30:28 -0800

    Still working on the math.  Travel time to the outside observer is correct.  Max velocity and ship time are still off.

--- travel.py
+++ travel.py
@@ -13,18 +13,19 @@ k = c ** 2 / a
 c = 2.99792458 * 10 ** 8 meters per sec (speed of light)
 a = acceleration / deceleration
 au = distance in AU
-d = au * (1.495978707 * 10 ** 11)
+d = au * 149597870700
+         149597870691
 
 
-traveltime = 2 * math.sqrt(k / a * (d // 2 * k + 1) - 1)
-days = traveltime / 86400
+traveltime = 2 * math.sqrt(k / a * math.floor((d / 2 * k + 1) ** 2) - 1)
+days = traveltime // 86400
 
 
 tsc = travel time from the ship's point of reference
 acosh = inverse hyperbolic cosine
 
 tsc = ((2 * c) / a ) * ( math.acosh * math.round( ( d / (2 * k ) ) + 1 ) )
-shipdays = tsc / 86400
+shipdays = tsc // 86400
 
 
 vamx = Maximum velocty at the halfway point (flip and decelerate)
@@ -36,7 +37,6 @@ problem has been solved by this point.  (Ex:  Epstien drive in The Expanse)
 '''
 import sys
 import math
-import numpy as np
 
 
 def safe_input():
@@ -50,7 +50,7 @@ def travel_time(a, d, k):
     '''
     Meat and potatoes of the back of the hand math to calculate travel time
     '''
-    traveltime = 2 * math.sqrt(k / a * (d // 2 * k + 1) - 1)
+    traveltime = 2 * math.sqrt(k / a * math.floor((d / 2 * k + 1) ** 2) - 1)
     return traveltime
 
 
@@ -58,7 +58,8 @@ def ship_time(a, c, d, k):
     '''
     This is the same as above, only for ship time
     '''
-    shiptime = np.arccosh(math.ceil((d / (2 * k))+ 1) * ((2 * c) / a))
+    shipa = math.floor((d / 2 * k) + 1)
+    shiptime = shipa * math.acosh(2 * c / a) ** -1
     return shiptime
 
 
@@ -71,17 +72,19 @@ def output(a, d):
 
     traveltime = travel_time(a, d, k) / 86400
     shiptime = ship_time(a, c, d, k) / 86400
-    vmax = c / math.sqrt(1 + (k / a * ((traveltime / 2) ** 2)))
+    vmax = c / math.sqrt(1 + k / (a * ((traveltime / 2)) ** 2)) / 1000
 
-    print(f'\nThis trip will take {traveltime} days, {shiptime} days of ship time. \
-The maximum velocity reached will be {vmax}.  \nClocks will be synced once docked.\n')
+    print(f'\nThis trip will take {str(traveltime)[:5]} days, {str(shiptime)[:5]}\
+days of ship time. \
+\nThe maximum velocity reached will be {vmax} kph.  \
+\n\nClocks will be synced once docked.\n')
 
 
 def custom():
     try:
         a = float(input('Enter the travel speed in Gs (ex: 1, or .33): '))
         au = float(input('Enter the travel distance in AUs (ex: .5 or 1): '))
-        d = au * (1.495978707 * 10 ** 11)
+        d = au * 149597870691
         output(a, d)
     except (KeyboardInterrupt, EOFError, ValueError):
         safe_input()
